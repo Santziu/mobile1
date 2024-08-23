a8800555d8aa0663064a53855f89f8ab82cfcc37
Wilson <wilsoncoutinh@gmail.com>
2024-08-23 18:34:56 -0300

    Pensar & Responder

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'modulo1',
+    loadChildren: () =>
+      import('./modulo1/modulo1.module').then((m) => m.Modulo1Module),
+  },
+  {
+    path: 'modulo2',
+    loadChildren: () =>
+      import('./modulo2/modulo2.module').then((m) => m.Modulo2Module),
+  },
+  { path: '', redirectTo: '/modulo1/child1', pathMatch: 'full' },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header.component';
+import { Child1Component } from './modulo1/child1/child1.component';
+import { Child2Component } from './modulo2/child2/child2.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
